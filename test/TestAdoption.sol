1f0521702fa4c74af4a3eba23bbaa7f27d10d97b
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-09-12 12:04:45 +0100

    Add test for the getAdopters method

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -9,17 +9,19 @@ contract TestAdoption {
 
   function testUserCanAdoptPet() {
     uint returnId = adoption.adopt(8);
-
     uint expected = 8;
-
     Assert.equal(returnId, expected, "Adoption of pet ID 8 should be recorded.");
   }
 
   function testGetAdopterAddressByPetId() {
     address expected = this;
-
     address adopter = adoption.adopters(8);
-
     Assert.equal(adopter, expected, "Owner of pet ID 8 should be recorded.");
+  }
+
+  function testGetAdopterAddressByPetIdInArray() {
+    address expected = this;
+    address[16] memory adopters = adoption.getAdopters();
+    Assert.equal(adopters[8], expected, "Owner of pet ID 8 present in the returned array.");
   }
 }
