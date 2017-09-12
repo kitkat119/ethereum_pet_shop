c264c1c701a62570b3dd9df8b3f62d19d844e475
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-09-12 11:12:18 +0100

    Add return adopters array function

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -5,9 +5,11 @@ contract Adoption {
 
   function adopt(uint petId) public returns (uint) {
   require(petId >= 0 && petId <= 15);
-
   adopters[petId] = msg.sender;
-
   return petId;
+  }
+
+  function getAdopters() public returns (address[16]) {
+    return adopters;
   }
 }
